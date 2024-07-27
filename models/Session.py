47842ead1c76c6c89a220ed804775cc5ca370688
D.B <derekb@baughcl.com>
2024-07-27 19:43:38 +0700

    clean up

--- models/Session.py
+++ models/Session.py
@@ -1,7 +1,7 @@
 class Session:
 
-    def __init__(self, net=None, protect=None, access=None):
-        self.ubnt_net = net
-        self.ubnt_protect = protect
-        self.ubnt_access = access
+    def __init__(self, **kwargs):
+        self.ubnt_net = kwargs.get('net')
+        self.ubnt_protect = kwargs('protect')
+        self.ubnt_access = kwargs('access')
 
